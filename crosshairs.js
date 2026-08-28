f74ea186911edcd3db698b48be45695b76acc6fc
kavish <kavishture@gmail.com>
2026-08-28 21:19:05 +0530

    Update crosshairs.js

--- crosshairs.js
+++ crosshairs.js
@@ -6,10 +6,14 @@
 //   image: "images/crosshairs/my-crosshair.png",
 //   code: "MY_VALORANT_CODE",
 //   tags: ["small", "cyan"]
-// }
+// 
+
+{
+  id: "my-crosshair",
+  name: "My Crosshair",
+  image: "images/crosshairs/my-crosshair.png",
+  code: "MY_VALORANT_CODE",
+  tags: ["small", "cyan"]
+}
 const CROSSHAIRS = [];
- {
-   id: "my-crosshair",
- name: "who",
- image: "images/crosshairs/my-crosshair.png",
-code: "MY_VALORANT_CODE",
+
