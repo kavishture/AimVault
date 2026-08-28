b9eab4f5a3a7aeb40d72a0667cd2b002dc453932
kavish <kavishture@gmail.com>
2026-08-28 21:37:18 +0530

    Update crosshairs.js

--- crosshairs.js
+++ crosshairs.js
@@ -1,7 +1,9 @@
 const CROSSHAIRS = [
   {
+    id: "my-crosshair",
     name: "dot",
     image: "https://cdn.phototourl.com/free/2026-08-28-3cbb6ce5-7c14-4b2c-aa1a-82c4fa65e1c4.png",
     code: "0;P;d;1;f;0;0t;4;0l;1;1o;0;0a;1;0f;0;1b;0",
+    tags: ["small", "cyan"]
   }
 ];
